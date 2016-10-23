b86f6a89f4af3e006ad9d7478043ed7a2b438f3a
victor <waterwolf@ukr.net>
2016-10-23 20:17:20 +0400

    Create services project

---
+++ HospitalRegistryServices/PatientCardService.cs
@@ -0,0 +1,38 @@
+﻿using HospitalRegistryData.Entities;
+using HospitalRegistryRepositories.implementation;
+
+namespace HospitalRegistryServices
+{
+    public class PatientCardService
+    {
+        private readonly PatientCardRecordRepository _patientCardRecordRepository
+            = new PatientCardRecordRepository();
+
+        private readonly ReceptionRepository _receptionRepository
+            = new ReceptionRepository();
+
+        public void StoreRecordFromString(string record)
+        {
+            _patientCardRecordRepository.Add(RecordFromString(record));
+        }
+
+        public PatientCardRecord RecordFromString(string record)
+        {
+            var splittedRecord = record.Split('.');
+            var receptionId = int.Parse(splittedRecord[0]);
+            var symptoms = splittedRecord[1];
+            var diagnosis = splittedRecord[2];
+            var prescription = splittedRecord[3];
+
+            var reception = _receptionRepository.Get(receptionId);
+
+            return new PatientCardRecord
+            {
+                Symptoms = symptoms, 
+                Diagnosis = diagnosis,
+                Prescription = prescription,
+                Reception = reception
+            };
+        }
+    }
+}
---
+++ HospitalRegistryServices/PatientService.cs
@@ -0,0 +1,41 @@
+﻿using HospitalRegistryData.Entities;
+using HospitalRegistryRepositories.implementation;
+
+namespace HospitalRegistryServices
+{
+    public class PatientService
+    {
+        private PatientRepository patientRepository = new PatientRepository();
+
+        public void AddPatient(string name, string surname)
+        {
+            var patient = new Patient {Name = name, Surname = surname};
+            patientRepository.Add(patient);
+        }
+
+        public Patient PatientFromString(string patientData)
+        {
+            var splittedData = patientData.Split(' ');
+            var name = splittedData[0];
+            var surname = splittedData[1];
+
+            return new Patient {Name = name, Surname = surname};
+        }
+
+        public void SavePatientFromString(string patientData)
+        {
+            var splittedData = patientData.Split(' ');
+            var name = splittedData[0];
+            var surname = splittedData[1];
+
+            var patient = new Patient { Name = name, Surname = surname };
+            patientRepository.Add(patient);
+        }
+
+        public void RemovePatient(int id)
+        {
+            var patient = patientRepository.Get(id);
+            patientRepository.Remove(patient);
+        }
+    }
+}
