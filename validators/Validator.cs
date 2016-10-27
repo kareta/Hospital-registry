93fd347d9cd0d9614cf306207f73830836efbc41
victor <waterwolf@ukr.net>
2016-10-28 03:04:44 +0400

    Create validators library

---
+++ validators/Validator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace viewshospital.validators
+{
+    public class Validator
+    {
+        //format-name pattern value
+        public List<string> GetErrorList(List<string> formats)
+        {
+            var errorList = new List<string>();
+
+            if (formats == null)
+            {
+                errorList.Add("Data is typed in a wrong format");
+                return errorList;
+            }
+
+            foreach (var format in formats)
+            {
+                var splittedFormat = format.Split(' ');
+
+                if (splittedFormat.Length != 3)
+                {
+                    continue;
+                }
+
+                var formatName = splittedFormat[0];
+                var pattern = splittedFormat[1];
+                var value = splittedFormat[2];
+
+                if (!Regex.IsMatch(value, pattern))
+                {
+                    errorList.Add(formatName + " is incorrect");
+                }
+            }
+            return errorList;
+        }
+    }
+}
